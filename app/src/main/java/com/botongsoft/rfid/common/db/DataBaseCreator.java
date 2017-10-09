9e6e45ba3a5b5e34d2faacd62a64d973ebcced43
nhy2721 <32147336@qq.com>
2017-10-09 14:11:51 +0800

    更改数据库存放路径

--- app/src/main/java/com/botongsoft/rfid/common/db/DataBaseCreator.java
+++ app/src/main/java/com/botongsoft/rfid/common/db/DataBaseCreator.java
@@ -1,12 +1,16 @@
 package com.botongsoft.rfid.common.db;
 
+import android.os.Environment;
+
 import com.botongsoft.rfid.BaseApplication;
 import com.lidroid.xutils.DbUtils;
 
 public class DataBaseCreator {
+	private static String PATH = Environment
+			.getExternalStorageDirectory().getAbsolutePath()+"/com.botongsoft.rfid/";
 	public static DbUtils create() {
 //		com_yt_payee_bean_classity_PayOrderInfo
-		DbUtils db = DbUtils.create(BaseApplication.context,
+		DbUtils db = DbUtils.create(BaseApplication.context,PATH,
 				BaseApplication.DBNAMESTRING, BaseApplication.DBVERSION, null);
 //		 DbUtils db = DbUtils.create(ApplicationApp.context);
 		//开启事务
