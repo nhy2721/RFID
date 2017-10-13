cc5f428760874ba4c1dae079ec2fb11672b0a553
nhy2721 <32147336@qq.com>
2017-10-13 15:04:47 +0800

    旧版本

--- app/src/main/java/com/botongsoft/rfid/ui/activity/SyncbakActivity.java
+++ app/src/main/java/com/botongsoft/rfid/ui/activity/SyncbakActivity.java
@@ -1490,7 +1490,7 @@ public class SyncbakActivity extends BaseActivity {
                 }
                 return true;
             case R.id.action_Sync:
-                if (isOnLine) {//网络 状态正常aaa
+                if (isOnLine) {//网络 状态正常aaaa
                     //                    showAnimate(item); //这里开始动画 太丑了
                     item.setEnabled(false);
                     bt_action1.setEnabled(false);
